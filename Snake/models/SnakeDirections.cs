3dc1a9893a780ed927d34b0a30fb4fbe28c5faf0
szajberpiraty <szajberpiraty@gmail.com>
2018-09-06 18:16:38 +0200

    #3 #4 Enum deklarálása, alapértéke

--- Snake/models/SnakeDirections.cs
+++ Snake/models/SnakeDirections.cs
@@ -1,7 +1,10 @@
 ﻿namespace Snake.models
 {
+    //Az enum tulajdonképpen egész számokat használ, és 0 az alapértelmezett értéke. Tetszőleges értékek megadhatóak.
+    //Ha olyan alapértelmezett érték kell, ami nem szól bele a többi értékbe, akkor adunk egy None-t
     public enum SnakeDirections
     {
+        None, //Alapértelmezett irány
         Left,
         Right,
         Up,
